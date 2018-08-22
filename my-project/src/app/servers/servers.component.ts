4d513824297e1e1e1c444152ffab9a32eb67c5db
Anastasiia Pelekh <anastasiia.pelekh@edvantis.com>
2018-08-22 14:26:34 +0300

    third task was done

--- my-project/src/app/servers/servers.component.ts
+++ my-project/src/app/servers/servers.component.ts
@@ -9,23 +9,38 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   servername = 'Testserver';
   serverCreationStatus = 'No server was created!';
+  serverCreated = false;
+  servers = ['Testserver', 'Testserver 2'];
 
+
+  // task2
   username = '';
   button = true;
+  // task2
+
+  // task3
+  toggleDetails = false;
+  count = 0;
+  arrDetails = [];
+  // task3
 
   constructor() {
-    // setTimeout(() => {
-    //   this.allowNewServer = true;
-    // },2000);
+    setTimeout(() => {
+      this.allowNewServer = true;
+    },2000);
   }
 
   ngOnInit() {
   }
 
   onCreationServer(){
+    this.serverCreated = true;
+    this.servers.push(this.servername);
     this.serverCreationStatus = 'Server was created! Name is' + this.servername;
   }
 
+
+  // task2
   resetB(){
     this.button = true;
     this.username = '';
@@ -35,4 +50,18 @@ export class ServersComponent implements OnInit {
       this.button = false;
     }
   }
+  // task2
+
+  // task3
+  onButtonDetails(){
+    this.toggleDetails = true;
+    this.count = this.count+1;
+    this.arrDetails.push(this.count);
+  }
+
+  styleDetails(detail){
+    return detail > 4 ? 'blue' : 'white';
+  }
+  // task3
+
 }
